cd7bce672ee460994ef5589ef43d4d441564f8c1
loganliew <s22702226@gmail.com>
2026-06-29 11:16:29 +0800

    Update update_data.py
    如果拿不到股票資訊, 則讓github也顯示fail

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import time
 import os
+import sys
 
 # =================================================================
 # 📂 1. 設定存檔路徑
@@ -126,7 +127,9 @@ def main():
     
     # 1. 獲取全市場名單
     all_stocks = get_all_tw_stocks()
-    if not all_stocks: return
+    if not all_stocks: 
+        print("❌ 無法獲取股票名單，強制中止執行並回報錯誤狀態給 GitHub！")
+        sys.exit(1) # 👈 這行就是讓 GitHub 亮紅燈的終極武器
     
     # 2. 比對出還沒抓過的清單
     todo_list = get_todo_list(all_stocks)
